2c005d490a86f184fa5c747e1365e4bc74613f8b
fairouz <fairouz.amoussa@epitech.eu>
2025-04-01 18:11:59 +0100

    Initial commit: setup Vue.js architecture with Vite, Vue Router and Tailwind CSS , correction of the eslintrc.js

---
+++ kps_groupeca_vue/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:vue/vue3-essential',
+    ],
+    parserOptions: {
+      requireConfigFile: false,
+    },
+    rules: {
+      // Tes règles personnalisées ici
+    },
+  }
+  
