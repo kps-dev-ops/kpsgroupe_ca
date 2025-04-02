b814138bfdf89a8c5c2ffba59bff36c8f1a6f1e4
fairouz <fairouz.amoussa@epitech.eu>
2025-04-02 14:54:38 +0100

    coorection of the deplucation of the acceuil page

--- kps/src/router/index.js
+++ kps/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Accueil from '../views/Acceuil.vue'
+// import Accueil from '../views/Acceuil.vue'
 import APropos from '../views/APropos.vue'
 import Contact from '../views/Contact.vue'
 import Expertise from '../views/Expertise.vue'
@@ -8,7 +8,7 @@ import Technologies from '../views/Technologies.vue'
 // import Contact from '../views/Contact.vue'
 
 const routes = [
-  { path: '/', name: 'Accueil', component: Accueil },
+//   { path: '/', name: 'Accueil', component: Accueil },
   { path: '/about', name: 'APropos', component: APropos },
   { path: '/contact', name: 'Contact', component: Contact },
   { path: '/expertise', name: 'Expertise', component: Expertise },
