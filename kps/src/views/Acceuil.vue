bd56bea08e7acf26660d241d392ac1dc711daa5a
masterskill <owens.aihunzoun@epitech.eu>
2025-04-02 11:46:38 +0100

    pull back of old version

--- kps/src/views/Acceuil.vue
+++ kps/src/views/Acceuil.vue
@@ -1,21 +1,18 @@
-<!-- <template>
-    <div>
-      <h1>Bienvenue sur la page d'accueil !</h1>
-      <HelloWorld />
-    </div>
-  </template>
-  
-  <script setup>
-  import HelloWorld from '../components/HelloWorld.vue'
-  </script>
-   -->
+<template>
+  <div>
+    <h1>{{ appStore.pageTitle }}</h1>
+    <HelloWorld />
+  </div>
+</template>
 
 <script setup>
+// Importation du store Pinia
 import { useAppStore } from '../stores/appStore'
+import HelloWorld from '../components/HelloWorld.vue'
+
+// Utilisation du store
 const appStore = useAppStore()
+
+// Mise à jour du titre de la page via le store
 appStore.setPageTitle('Accueil')
 </script>
-
-<template>
-  <h1>{{ appStore.pageTitle }}</h1>
-</template>
