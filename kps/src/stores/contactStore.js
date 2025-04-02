3003a449c87dc8001a9d7be277bc180c3c5f5627
masterskill <owens.aihunzoun@epitech.eu>
2025-04-02 16:05:04 +0100

    push of the pinia store for contacts

--- kps/src/stores/contactStore.js
+++ kps/src/stores/contactStore.js
@@ -1,19 +1,43 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
 export const useContactStore = defineStore('contact', {
   state: () => ({
-    formData: {
-      name: '',
-      email: '',
-      message: ''
-    }
-  }),
-  actions: {
-    updateFormData(field, value) {
-      this.formData[field] = value
-    },
-    resetForm() {
-      this.formData = { name: '', email: '', message: '' }
-    }
-  }
-})
+    sectionTitle: 'Contactez-nous',
+    sectionDescription: 'Besoin d’informations ? Notre équipe est à votre disposition pour répondre à toutes vos questions.',
+    contactInfos: [
+      {
+        title: 'Adresse',
+        value: '25 Rue de Ponthieu, 75008 Paris',
+        icon: 'bi bi-geo-alt',
+        delay: 300
+      },
+      {
+        title: 'Numéros de téléphone',
+        value: ['+33 6 52 521 414', '+33 6 61 600 900', '+33 6 51 953 090'],
+        icon: 'bi bi-telephone',
+        delay: 400
+      },
+      {
+        title: 'Email',
+        value: 'contact@kpsgroupe.com',
+        link: 'mailto:contact@kpsgroupe.com',
+        icon: 'bi bi-envelope',
+        delay: 500
+      },
+      {
+        title: 'Site Web',
+        value: 'https://kpsgroupe.ma/',
+        link: 'https://kpsgroupe.ma/',
+        icon: 'bi bi-globe',
+        delay: 600
+      },
+      {
+        title: 'LinkedIn',
+        value: 'Suivez-nous sur LinkedIn',
+        link: 'https://www.linkedin.com/company/kps-analytics/',
+        icon: 'bi bi-linkedin',
+        delay: 700
+      }
+    ]
+  })
+});
