0a6c543e01d5d3c266f8e1d60dbf8ce9f03661d2
masterskill <owens.aihunzoun@epitech.eu>
2025-04-02 16:05:59 +0100

    pus of the first version of Expertise vue

--- kps/src/views/Expertise.vue
+++ kps/src/views/Expertise.vue
@@ -1,10 +1,44 @@
 <template>
-    <div class="expertise">
-      <h1>Notre Expertise</h1>
-      <p>Voici les domaines d’expertise de notre équipe...</p>
+  <section id="services" class="services section light-background">
+    <!-- Section Title -->
+    <div class="container section-title" data-aos="fade-up">
+      <h2>{{ servicesStore.sectionTitle }}</h2>
+      <p>{{ servicesStore.sectionDescription }}</p>
     </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
+    
+    <!-- Services List -->
+    <div class="container" data-aos="fade-up" data-aos-delay="100">
+      <div class="row g-4">
+        <div
+          v-for="service in servicesStore.services"
+          :key="service.id"
+          class="col-lg-6"
+          :data-aos="'fade-up'"
+          :data-aos-delay="service.delay"
+        >
+          <div class="service-card d-flex">
+            <div class="icon flex-shrink-0">
+              <i :class="service.icon"></i>
+            </div>
+            <div>
+              <h3>{{ service.title }}</h3>
+              <p>{{ service.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { useServicesStore } from '../stores/expertiseStore'
+
+export default {
+  name: 'Services',
+  setup() {
+    const servicesStore = useServicesStore()
+    return { servicesStore }
+  }
+}
+</script>
