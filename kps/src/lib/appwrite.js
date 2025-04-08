757a3036f0b789c86b6cae4e926646ccd341d8c2
masterskill <owens.aihunzoun@epitech.eu>
2025-04-08 17:11:20 +0100

    correct of the project databases id

--- kps/src/lib/appwrite.js
+++ kps/src/lib/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account} from 'appwrite';
 export const client = new Client();
 
 client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('<PROJECT_ID>');
+  .setEndpoint('https://appwrite.ubbfy.com/v1')
+  .setProject('67f3ad4f00234f8ab06c');
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
