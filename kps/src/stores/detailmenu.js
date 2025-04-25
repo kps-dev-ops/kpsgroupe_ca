8a801ea086ea1650f9d5bf9bf75f766f52db190a
fairouz <fairouz.amoussa@epitech.eu>
2025-04-25 09:44:19 +0100

    correction of the image

---
+++ kps/src/stores/detailmenu.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia'
+
+export const detailmenu = defineStore('header', {
+  state: () => ({
+    logoPath: './src/assets/img/logo2.png',
+    menu: [
+      { label: 'Accueil', href: '/' },
+   
+    ],
+    contactText: 'Contactez-nous',
+    contactHref: '#contact',
+    isMenuOpen: false
+  }),
+
+  actions: {
+    toggleMenu() {
+      console.log('toggleMenu called!') 
+      this.isMenuOpen = !this.isMenuOpen
+      console.log('isMenuOpen:', this.isMenuOpen)
+    },
+
+    updateMenu(newMenu) {
+      this.menu = newMenu
+    },
+
+    updateLogo(newPath) {
+      this.logoPath = newPath
+    },
+    resetMenu() {
+        this.menu = []
+      }
+  }
+})
