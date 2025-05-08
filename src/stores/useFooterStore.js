854c91a94209e333c27b61928f459f673a44c70c
fairouz <fairouz.amoussa@epitech.eu>
2025-05-08 10:09:06 +0100

    adding of the facebook logo

--- src/stores/useFooterStore.js
+++ src/stores/useFooterStore.js
@@ -14,6 +14,9 @@ export const useFooterStore = defineStore('footer', {
     socialLinks: [
       { icon: 'bi bi-linkedin', href: 'https://www.linkedin.com/company/kps-analytics/' }
     ],
+    socialFacebook: [
+      { icon: 'bi bi-facebook', href: 'https://www.facebook.com/kpsgroupe/' }
+    ],
     contact: {
       adresse: '25 Rue de Ponthieu, 75008 Paris',
       tel: '+33 6 52 521 414 / +33 6 61 600 900 / +33 6 51 953 090',
